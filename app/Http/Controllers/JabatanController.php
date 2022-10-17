f30380a4746cd5c433dc44ef1707d69144b60f7d
DESKTOP-BDQCLOB\hp <niyanhafizah@gmail.com>
2022-10-17 09:52:30 +0700

    Commit by niya

--- app/Http/Controllers/JabatanController.php
+++ app/Http/Controllers/JabatanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Jabatan;
 use Illuminate\Http\Request;
+use DB;
 
 class JabatanController extends Controller
 {
@@ -26,7 +27,8 @@ class JabatanController extends Controller
      */
     public function create()
     {
-        return view('Jabatan.tambahjabatan');
+         $jabatan = Jabatan::all();
+        return view('karyawan.tambah', ['jabatan' => $jabatan]);
     }
 
     /**
